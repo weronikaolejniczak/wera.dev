60b6d1c4d822dad72377fe1a8086092669785550
Weronika Olejniczak <wer.olejniczak@gmail.com>
2024-05-19 10:39:46 +0200

    chore: update site title and description constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'wero.dev | Weronika Olejniczak Portfolio';
+export const SITE_DESCRIPTION = 'Weronika Olejniczak, Software Engineer Portfolio';
